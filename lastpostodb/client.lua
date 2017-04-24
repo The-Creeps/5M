c69a86a47ad2b667aa00f3f0418bf08d85f4ce33
The-Creeps <involvedcreeps@gmail.com>
2017-04-24 14:38:34 +0200

    Publication
    Publication de la ressouce lastpostodb

---
+++ lastpostodb/client.lua
@@ -0,0 +1,28 @@
+--Boucle Thread d'envoie de la position toutes les x secondes vers le serveur pour effectuer la sauvegarde
+Citizen.CreateThread(function ()
+	while true do
+	--Durée entre chaque requêtes : 60000 = 60 secondes
+	Citizen.Wait(60000)
+		--Récupération de la position x, y, z du joueur
+		LastPosX, LastPosY, LastPosZ = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
+		--Récupération de l'azimut du joueur
+	    local LastPosH = GetEntityHeading(GetPlayerPed(-1))
+		--Envoi des données vers le serveur
+	    TriggerServerEvent("project:savelastpos", LastPosX , LastPosY , LastPosZ, LastPosH)
+	end
+end)
+
+--Event permetant au serveur d'envoyez une notification au joueur
+RegisterNetEvent('project:notify')
+AddEventHandler('project:notify', function(alert)
+    if not origin then
+        Notify(alert)
+    end
+end)
+
+--Notification joueur
+function Notify(text)
+    SetNotificationTextEntry('STRING')
+    AddTextComponentString(text)
+    DrawNotification(false, false)
+end
---
+++ lastpostodb/server.lua
@@ -0,0 +1,46 @@
+require "resources/essentialmode/lib/MySQL"
+--Configuration de la connexion vers la DB MySQL
+MySQL:open("127.0.0.1", "database", "login", "password")
+
+--Intégration de la position dans MySQL
+RegisterServerEvent("project:savelastpos")
+AddEventHandler("project:savelastpos", function( LastPosX , LastPosY , LastPosZ , LastPosH )
+	TriggerEvent('es:getPlayerFromId', source, function(user)
+		--Récupération du SteamID.
+		local player = user.identifier
+		--Formatage des données en JSON pour intégration dans MySQL.
+		local LastPos = "{\"" .. LastPosX .. "\", \"" .. LastPosY .. "\",  \"" .. LastPosZ .. "\", \"" .. LastPosH .. "\"}"
+		--Exécution de la requêtes SQL.
+		local executed_query = MySQL:executeQuery("UPDATE users SET `lastpos`='@lastpos' WHERE identifier = '@username'", {['@username'] = player, ['@lastpos'] = LastPos})
+		--Affichage d'un message confirmant la sauvegarde de la position du joueurs.
+		TriggerClientEvent("project:notify", -1, "Position Sauvegardée")
+	end)
+end)
+
+
+--Récupération de la position depuis MySQL (Non activé ! c'est juste pour montrer comment on récupére les infos de la DB)
+RegisterServerEvent("project:SpawnPlayer")
+AddEventHandler("project:SpawnPlayer", function()
+	TriggerEvent('es:getPlayerFromId', source, function(user)
+		--Récupération du SteamID.
+		local player = user.identifier
+		--Exécution de la requêtes SQL.
+		local executed_query = MySQL:executeQuery("SELECT * FROM users WHERE identifier = '@username'", {['@username'] = player})
+		--Récupération des données générée par la requête.
+		local result = MySQL:getResults(executed_query, {'lastpos'}, "identifier")
+		-- Vérification de la présence d'un résultat avant de lancer le traitement.
+		if(result)then
+			for k,v in ipairs(result)do
+				if v.lastpos ~= "" then
+				-- Décodage des données récupérée
+				local ToSpawnPos = json.decode(v.lastpos)
+				-- Intégration des données dans les variables dédiée
+				local PosX = ToSpawnPos[1]
+				local PosY = ToSpawnPos[2]
+				local PosZ = ToSpawnPos[3]
+				local PosH = ToSpawnPos[4]
+				end
+			end
+		end
+	end)
+end)
