3e1496d467c6e1678f01613aae69012d3e0f67fa
The-Creeps <involvedcreeps@gmail.com>
2017-06-14 16:04:02 +0200

    Mysql Async
    Mise à jour vers Mysql Async

--- lastpostodb/server.lua
+++ lastpostodb/server.lua
@@ -1,7 +1,5 @@
---Version 1.4
-require "resources/essentialmode/lib/MySQL"
---Configuration de la connexion vers la DB MySQL
-MySQL:open("127.0.0.1", "database", "login", "password")
+--Version 1.5
+require "resources/mysql-async/lib/MySQL"
 
 --Déclaration des EventHandler
 RegisterServerEvent("projectEZ:savelastpos")
@@ -15,29 +13,23 @@ AddEventHandler("projectEZ:savelastpos", function( LastPosX , LastPosY , LastPos
 		--Formatage des données en JSON pour intégration dans MySQL.
 		local LastPos = "{" .. LastPosX .. ", " .. LastPosY .. ",  " .. LastPosZ .. ", " .. LastPosH .. "}"
 		--Exécution de la requêtes SQL.
-		local executed_query = MySQL:executeQuery("UPDATE users SET `lastpos`='@lastpos' WHERE identifier = '@username'", {['@username'] = player, ['@lastpos'] = LastPos})
+		MySQL.Async.execute("UPDATE users SET `lastpos`=@lastpos WHERE identifier = @username", {['@username'] = player, ['@lastpos'] = LastPos})
 	end)
 end)
 
---Récupération de la position depuis MySQL
 AddEventHandler("projectEZ:SpawnPlayer", function()
 	TriggerEvent('es:getPlayerFromId', source, function(user)
 		--Récupération du SteamID.
 		local player = user.identifier
-		--Exécution de la requêtes SQL.
-		local executed_query = MySQL:executeQuery("SELECT * FROM users WHERE identifier = '@username'", {['@username'] = player})
 		--Récupération des données générée par la requête.
-		local result = MySQL:getResults(executed_query, {'lastpos'}, "identifier")
+		local result = MySQL.Sync.fetchScalar("SELECT lastpos FROM users WHERE identifier = @username", {['@username'] = player})	
 		-- Vérification de la présence d'un résultat avant de lancer le traitement.
-		if(result)then
-			for k,v in ipairs(result)do
-				if v.lastpos ~= "" then
+		if(result ~= nil)then
 				-- Décodage des données récupérées
-				local ToSpawnPos = json.decode(v.lastpos)
+				local ToSpawnPos = json.decode(result)
+				print(ToSpawnPos[1])
 				-- On envoie la derniere position vers le client pour le spawn
 				TriggerClientEvent("projectEZ:spawnlaspos", source, ToSpawnPos[1], ToSpawnPos[2], ToSpawnPos[3])
-				end
-			end
 		end
 	end)
 end)
